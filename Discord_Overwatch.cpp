646a6128a1846755d6ffb0baf5d12586f45f3faf
Xemuth <hamon.clement@outlook.fr>
2019-05-14 23:09:48 +0200

    modif mineur

--- Discord_Overwatch.cpp
+++ Discord_Overwatch.cpp
@@ -10,7 +10,7 @@ void Discord_Overwatch::getStats(ValueMap payload){
     String discriminator = payload["d"]["author"]["discriminator"];
 	
 	Cout() << "Event Overwatch" <<"\n";
-	ptrBot->CreateMessage(channel, "Event Overwatch !");
+	ptrBot->CreateMessage(channel, "Event " + name +" !");
 }
 	
 Discord_Overwatch::Discord_Overwatch(Upp::String _name, Upp::String _prefix){
