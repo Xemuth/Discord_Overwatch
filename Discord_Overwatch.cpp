b048511bde9514f670fa5dc70a0f44eae2a71844
Xemuth <hamon.clement@outlook.fr>
2019-05-14 23:06:56 +0200

    Ajout filtre via prefixe

--- Discord_Overwatch.cpp
+++ Discord_Overwatch.cpp
@@ -3,13 +3,20 @@
 using namespace Upp;
 
 void Discord_Overwatch::getStats(ValueMap payload){
-	Cout() << "Event déclanché ! #Overwatch" <<"\n";
+	String channel  = payload["d"]["channel_id"];
+    String content  = payload["d"]["content"];
+    String userName = payload["d"]["author"]["username"];
+    String id = payload["d"]["author"]["id"];
+    String discriminator = payload["d"]["author"]["discriminator"];
+	
+	Cout() << "Event Overwatch" <<"\n";
+	ptrBot->CreateMessage(channel, "Event Overwatch !");
 }
 	
 Discord_Overwatch::Discord_Overwatch(Upp::String _name, Upp::String _prefix){
 	name = _name;
 	prefix = _prefix;
-
+	
 	Events.Add([&](ValueMap e){this->getStats(e);});
 }
 
