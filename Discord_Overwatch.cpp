72b81c4f185a6e7ee07c6020710167052b013140
xemuth <hamonclem@hotmail.fr>
2019-06-23 15:54:50 +0200

    Ajout de plusieurs fonc

--- Discord_Overwatch.cpp
+++ Discord_Overwatch.cpp
@@ -1,7 +1,7 @@
 #include "Discord_Overwatch.h"
 #include <Sql/sch_schema.h>
 #include <Sql/sch_source.h>
-
+#include <iostream>
 using namespace Upp;
 
 void Discord_Overwatch::getStats(ValueMap payload){
@@ -14,15 +14,13 @@ void Discord_Overwatch::getStats(ValueMap payload){
 }
 
 void Discord_Overwatch::executeSQL(ValueMap payload){
-	Upp::String message = payload["d"]["content"];
-	message.Replace(String("!" +prefix +" "),"");
-	if(message.GetCount() >7 &&  message.Find("ExecSQL")!=-1){
+	if(Message.GetCount() >7){
 		if(bddLoaded){
 			Sql sql;
-			message.Replace(String("ExecSQL "),"");
-			message+=";";
-			if(message.Find("SELECT")!=-1){
-				sql.Execute(message);
+			Message.Replace(String("ExecSQL "),"");
+			Message+=";";
+			if(Message.Find("SELECT")!=-1){
+				sql.Execute(Message);
 				int cpt=0;
 				Upp::String result = "";
 				while(sql.Fetch()){
@@ -37,7 +35,7 @@ void Discord_Overwatch::executeSQL(ValueMap payload){
 				if(result.GetCount() == 0) result <<"No values";
 				ptrBot->CreateMessage(channelLastMessage,result);	
 			}else{
-				sql.Execute(message);
+				sql.Execute(Message);
 				ptrBot->CreateMessage(channelLastMessage,sql.ToString());
 			}
 		}else{
@@ -47,15 +45,10 @@ void Discord_Overwatch::executeSQL(ValueMap payload){
 }
 
 void Discord_Overwatch::testApi(ValueMap payload){
-	String channel  = payload["d"]["channel_id"];
-    String id = payload["d"]["author"]["id"];
-	Upp::String message = payload["d"]["content"];
-	message.Replace(String("!" +prefix +" "),"");
-	Vector<String> args = Split(message," ");
-	if( args.GetCount() == 2 &&  args[0].Compare("api")==0){
+	if( MessageArgs.GetCount() == 2 ){
 		HttpRequest reqApi;
-		args[1].Replace("#","-");
-		reqApi.Url("https://ow-api.com/v1/stats/pc/euw/" + args[1] + "/profile");
+		MessageArgs[1].Replace("#","-");
+		reqApi.Url("https://ow-api.com/v1/stats/pc/euw/" + MessageArgs[1] + "/profile");
 		reqApi.GET();
 		auto json = ParseJSON(reqApi.Execute());
 		ptrBot->CreateMessage(channelLastMessage,json["rating"].ToString());
@@ -63,43 +56,158 @@ void Discord_Overwatch::testApi(ValueMap payload){
 		ptrBot->CreateMessage(channelLastMessage,"Error url");
 	}
 }
+
+//!ow register BASTION#21406 Clément
+void Discord_Overwatch::Register(ValueMap payload){// The main idea is " You must be registered to be addable to an equipe or create Equipe
+	if(MessageArgs.GetCount()>2){
+		try{
+			Sql sql;
+			if(MessageArgs.GetCount() == 3){
+				sql*Insert(OW_PLAYERS)(BATTLE_TAG,MessageArgs[1])(DISCORD_ID,AuthorId)(COMMUN_NAME,MessageArgs[2]);
+				ptrBot->CreateMessage(channelLastMessage,"Enregistrement réussie");
+			}else if (MessageArgs.GetCount() == 2){
+				sql*Insert(OW_PLAYERS)(BATTLE_TAG,MessageArgs[1])(DISCORD_ID,AuthorId)(COMMUN_NAME,"");
+				ptrBot->CreateMessage(channelLastMessage,"Enregistrement réussie");	
+			}else
+				ptrBot->CreateMessage(channelLastMessage,"Error Registering");
+		}catch(...){
+			ptrBot->CreateMessage(channelLastMessage,"Error Registering");
+		}
+	}
+}
+
+//!ow remove 
+void Discord_Overwatch::DeleteProfil(ValueMap payload){ //Remove user from the bdd 
+	if(MessageArgs.GetCount()==1){
+		try{
+			Sql sql;
+			sql*Delete(OW_PLAYERS).Where(DISCORD_ID == AuthorId);
+			ptrBot->CreateMessage(channelLastMessage,"Supression reussie");
+		}catch(...){
+			ptrBot->CreateMessage(channelLastMessage,"Error deletion");
+		}
+	}
+}
+
+//!ow createEquipe Sombre est mon histoire
+void Discord_Overwatch::CreateEquipe(ValueMap payload){ //To add an equipe you must be registered. when an equipe is created, only 
+	if(MessageArgs.GetCount()>2 && isRegestered(AuthorId)){
+		try{
+			String teamName="";
+			for(int e =1; e < MessageArgs.GetCount(); e++){
+				if(e!=1 && e !=  MessageArgs.GetCount()-1)teamName << " ";
+				teamName << MessageArgs[e];
+			}
+			Sql sql;
+			if(sql*Insert(OW_EQUIPES)(EQUIPE_NAME,teamName)){
+				int idInserted = 0;
+				idInserted =std::stoi( sql.GetInsertedId().ToString().ToStd());
+				if(idInserted !=0){
+					if(sql*Insert(OW_EQUIPES_CREATOR)(EQUIPES_ID,idInserted)(CREATOR_DISCORD_ID,AuthorId)){
+						ptrBot->CreateMessage(channelLastMessage,"Ajout d'équipe reussie");
+						return;
+					}
+				}
+			}
+			ptrBot->CreateMessage(channelLastMessage,"Erreur création team");
+		}catch(...){
+			ptrBot->CreateMessage(channelLastMessage,"Erreur création team");
+		}
+	}else{
+		ptrBot->CreateMessage(channelLastMessage,"You must be registered Before creating team ! ");
+	}
+}
+
+//!ow removeEquipe Sombre est mon histoire 
+void Discord_Overwatch::RemoveEquipe(ValueMap payload){ //you must have the right to remove equipe
+}
+
+//!ow AddRight @NattyRoots Sombre est mon histoire
+void Discord_Overwatch::GiveRight(ValueMap payload){ //Allow equipe owner/ equipe righter to add person to the equipe
+}
+
+//!ow RemoveRight @NattyRoots Sombre est mon histoire
+void Discord_Overwatch::RemoveRight(ValueMap payload){ //Remove equipe righter to one personne By discord ID
+}
+
+//!ow AddPerson @NattyRoots Sombre est mon histoire
+void Discord_Overwatch::AddPersonToEquipe(ValueMap payload){ //To add a person to an equipe you must have the right to add it
+}
+
+//!ow RemovePerson @NattyRoots Sombre est mon histoire
+void Discord_Overwatch::RemovePersonToEquipe(ValueMap payload){ //Remove Person from and equipe (only righter can do it)
+}
+
+//!ow upd
+void Discord_Overwatch::ForceUpdate(ValueMap payload){ //Force update, based on the personne who make the call
+}
+
+//!ow Eupd Sombre est mon histoire
+void Discord_Overwatch::ForceEquipeUpdate(ValueMap payload){ // Idk if only ppl who have right on equipe must do it or letting it free.
 	
-	
+}
+
+bool Discord_Overwatch::isRegestered(String Id){
+	Sql sql;
+	S_OW_PLAYERS row;
+	sql*Select(row).From(OW_PLAYERS).Where(DISCORD_ID == Id);
+	if(sql.Fetch())return true;
+	return false;
+}
+
 Discord_Overwatch::Discord_Overwatch(Upp::String _name, Upp::String _prefix){
 	name = _name;
 	prefix = _prefix;
 	
 	prepareOrLoadBDD();
-	EventsMap.Add([&](ValueMap e){this->getStats(e);});
-	EventsMap.Add([&](ValueMap e){this->executeSQL(e);});
-	EventsMap.Add([&](ValueMap e){this->testApi(e);});
+	EventsMap.Add([&](ValueMap e){if(MessageArgs[0].IsEqual("test"))this->getStats(e);});
+	EventsMap.Add([&](ValueMap e){if(MessageArgs[0].IsEqual("execsql"))this->executeSQL(e);});
+	EventsMap.Add([&](ValueMap e){if(MessageArgs[0].IsEqual("api"))this->testApi(e);});
+	EventsMap.Add([&](ValueMap e){if(MessageArgs[0].IsEqual("register"))this->Register(e);});
+	EventsMap.Add([&](ValueMap e){if(MessageArgs[0].IsEqual("remove"))this->DeleteProfil(e);});
+	EventsMap.Add([&](ValueMap e){if(MessageArgs[0].IsEqual("createequipe"))this->CreateEquipe(e);});
 }
 
-void Discord_Overwatch::Events(ValueMap payload){
+void Discord_Overwatch::Events(ValueMap payload){ //We admit BDD must be loaded to be active
 	channelLastMessage =  payload["d"]["channel_id"]; //HEre we hook chan  
+	AuthorId = payload["d"]["author"]["id"];
+	Message = payload["d"]["content"];
+	Message.Replace(String("!" +prefix +" "),"");
+	MessageArgs = Split(Message," ");
+	MessageArgs[0] = ToLower(MessageArgs[0]);
 	for(auto &e : EventsMap){
-		e(payload);
+		if(bddLoaded){
+			e(payload);
+		}
+		else{ 
+			ptrBot->CreateMessage(channelLastMessage,"DataBase not loaded !"); 
+			break;
+		}
 	}
 }
 
 void Discord_Overwatch::prepareOrLoadBDD(){
 	bddLoaded =false;
 	sqlite3.LogErrors(true);
+	bool mustCreate = false;
+	if(!FileExists("Overwatch_DataBase.db"))mustCreate =true;
 	if(sqlite3.Open("Overwatch_DataBase.db")) {
 		SQL = sqlite3;
 		#ifdef _DEBUG
-			SqlSchema sch(SQLITE3);
-			All_Tables(sch);
-			if(sch.ScriptChanged(SqlSchema::UPGRADE))
-				SqlPerformScript(sch.Upgrade());
-			if(sch.ScriptChanged(SqlSchema::ATTRIBUTES)) {	
-				SqlPerformScript(sch.Attributes());
+			if(mustCreate){
+				SqlSchema sch(SQLITE3);
+				All_Tables(sch);
+				if(sch.ScriptChanged(SqlSchema::UPGRADE))
+					SqlPerformScript(sch.Upgrade());
+				if(sch.ScriptChanged(SqlSchema::ATTRIBUTES)) {	
+					SqlPerformScript(sch.Attributes());
+				}
+				if(sch.ScriptChanged(SqlSchema::CONFIG)) {
+					SqlPerformScript(sch.ConfigDrop());
+					SqlPerformScript(sch.Config());
+				}
+				sch.SaveNormal();
 			}
-			if(sch.ScriptChanged(SqlSchema::CONFIG)) {
-				SqlPerformScript(sch.ConfigDrop());
-				SqlPerformScript(sch.Config());
-			}
-			sch.SaveNormal();
 		#endif
 		bddLoaded =true;
 	}
--- Discord_Overwatch.h
+++ Discord_Overwatch.h
@@ -14,11 +14,41 @@ class Discord_Overwatch: public DiscordModule{
 		bool bddLoaded = false;
 		Sqlite3Session sqlite3; //DataBase
 		String channelLastMessage=""; //hook to latest message chan 
-		
+	    String AuthorId =""; 
+	    String Message ="";
+		Vector<String> MessageArgs;
+
+		//Test
 		void getStats(ValueMap payload);
 		void executeSQL(ValueMap payload);
 		void testApi(ValueMap payload);
 		
+		//!ow register BASTION#21406 Clément
+		void Register(ValueMap payload);// The main idea is " You must be registered to be addable to an equipe or create Equipe
+		//!ow remove 
+		void DeleteProfil(ValueMap payload); //Remove user from the bdd 
+		
+		//!ow createEquipe Sombre est mon histoire
+		void CreateEquipe(ValueMap payload); //To add an equipe you must be registered. when an equipe is created, only 
+		//!ow removeEquipe Sombre est mon histoire 
+		void RemoveEquipe(ValueMap payload); //you must have the right to remove equipe
+		//!ow AddRight @NattyRoots Sombre est mon histoire
+		void GiveRight(ValueMap payload); //Allow equipe owner/ equipe righter to add person to the equipe
+		//!ow RemoveRight @NattyRoots Sombre est mon histoire
+		void RemoveRight(ValueMap payload); //Remove equipe righter to one personne By discord ID
+		
+		//!ow AddPerson @NattyRoots Sombre est mon histoire
+		void AddPersonToEquipe(ValueMap payload); //To add a person to an equipe you must have the right to add it
+		//!ow RemovePerson @NattyRoots Sombre est mon histoire
+		void RemovePersonToEquipe(ValueMap payload); //Remove Person from and equipe (only righter can do it)
+		
+		//!ow upd
+		void ForceUpdate(ValueMap payload); //Force update, based on the personne who make the call
+		//!ow Eupd Sombre est mon histoire
+		void ForceEquipeUpdate(ValueMap payload); // Idk if only ppl who have right on equipe must do it or letting it free.
+		
+		bool isRegestered(String Id);
+		void RetrieveData(); //USed to refresh all team 
 		void prepareOrLoadBDD(); //Used to load BDD
 	public:
 		Discord_Overwatch(Upp::String _name, Upp::String _prefix);
