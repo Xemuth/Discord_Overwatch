3a1aed2f82c32c8a1e4e03d78d24f5bdad13b3ff
Xemuth <hamon.clement@outlook.fr>
2019-07-10 23:51:39 +0200

    Correction pour linux

--- Discord_Overwatch.h
+++ Discord_Overwatch.h
@@ -35,7 +35,6 @@ at the end of Database loading function
 class Discord_Overwatch: public DiscordModule{
 	private:
 		bool bddLoaded = false;
-		bool ActionDone = false;
 		Sqlite3Session sqlite3; //DataBase
 
 		Vector<Equipe> equipes;
